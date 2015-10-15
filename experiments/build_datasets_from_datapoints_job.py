29d5b9e80fae055a4ce16d3c309e0ef46501f150
Pablo G Celayes <pablocelayes@gmail.com>
2015-10-15 20:16:43 -0300

    fixed import error

--- experiments/build_datasets_from_datapoints_job.py
+++ experiments/build_datasets_from_datapoints_job.py
@@ -1,4 +1,4 @@
-from experiments.datasets import build_datapoints_job
+from experiments.datasets import build_dataset_from_datapoints_job
 
 if __name__ == '__main__':
     build_dataset_from_datapoints_job()
