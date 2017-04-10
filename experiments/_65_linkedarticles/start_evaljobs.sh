064d57a1ebc92db5cca2a58ba82d08a59c392efc
Pablo G Celayes <pablocelayes@gmail.com>
2017-04-10 18:55:35 -0300

    removimos experimentos q no van a la tesis

--- experiments/_65_linkedarticles/start_evaljobs.sh
+++
@@ -1,6 +0,0 @@
-python evaluate.py 0 > report0.log &
-python evaluate.py 1 > report1.log &
-python evaluate.py 2 > report2.log &
-# python evaluate.py 3 > report3.log &
-# python evaluate.py 4 > report4.log &
-# python evaluate.py 5 > report5.log &
--- experiments/build_dataset_from_datapoints_job.py
+++
@@ -1,4 +0,0 @@
-from experiments.datasets import build_dataset_from_datapoints_job
-
-if __name__ == '__main__':
-    build_dataset_from_datapoints_job()
