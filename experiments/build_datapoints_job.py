19357eb48ad53242e90ab545f5e2047bf679a7d0
Pablo G Celayes <pablocelayes@gmail.com>
2017-06-10 12:25:18 -0300

    limpiamos experimentos y archivos no usados en el desarrollo del paper

--- experiments/build_datapoints_job.py
+++
@@ -1,4 +0,0 @@
-from experiments.datasets import build_datapoints_job
-
-if __name__ == '__main__':
-    build_datapoints_job()
--- experiments/startdatasetjobs.sh
+++
@@ -1,8 +0,0 @@
-# nohup python build_dataset_from_datapoints_job.py 0 train >  datasetjob0.out &
-# nohup python build_dataset_from_datapoints_job.py 1 train >  datasetjob1.out &
-# nohup python build_dataset_from_datapoints_job.py 2 train >  datasetjob2.out &
-# nohup python build_dataset_from_datapoints_job.py 3 train >  datasetjob3.out &
-# nohup python build_dataset_from_datapoints_job.py 4 train >  datasetjob4.out &
-# nohup python build_dataset_from_datapoints_job.py 5 train >  datasetjob5.out &
-nohup python build_dataset_from_datapoints_job.py 6 train > datasetjob6.out &
-nohup python build_dataset_from_datapoints_job.py 7 train > datasetjob7.out &
